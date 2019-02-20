f84582366ca545d79e251c8f79f251d746ed279b
Nabeel Valley <nabeelvalley1@hotmail.com>
2019-02-20 21:15:32 +0200

    :bug: still trying to fix the deploy bug

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3001
 
-app.use(express.static(path.join(__dirname,'../app/build')))
-
 app.get('/api/test', (req, res) => res.send('<h1>Test Successful<h2>'))
 
 app.use('/api/times', require(path.join(__dirname, './routes/times')))
@@ -13,5 +11,7 @@ app.use('/api/index', require(path.join(__dirname, './routes/index')))
 app.get('*', (req, res) => {
   res.redirect('/')
 })
+
+app.use(express.static(path.join(__dirname,'../app/build')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
