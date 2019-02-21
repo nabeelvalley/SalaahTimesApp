5158ade33278b496988ced4daf75947d98d8512e
Nabeel Valley <nabeelvalley1@hotmail.com>
2019-02-21 12:25:41 +0200

    :rocket: add app name to deploy script

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t salaah-times .
-heroku container:push web
-heroku container:release web
+heroku container:push web --app=salaah-times
+heroku container:release web  --app=salaah-times
