b951e85716442e807b6a1eb6c9ffd8b482cda19d
Nabeel Valley <nabeelvalley1@hotmail.com>
2019-02-21 11:22:29 +0200

    :memo: do not redirect https for localhost

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const path = require('path')
-var enforce = require('express-sslify');
+var enforce = require('express-sslify')
 const app = express()
 const port = process.env.PORT || 3001
 
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
+app.use(function(req, res, next){
+  const host = req.headers.host
+  const isLocal = Boolean(
+    host.includes('localhost') ||
+      host.includes('[::1]') ||
+      host.match(/(\d*\.){3}(\d*){1}/)
+  )
+  return isLocal? next() : enforce.HTTPS({ trustProtoHeader: true })(req, res, next)
+})
 
-app.use(express.static(path.join(__dirname,'../app/build/')))
+app.use(express.static(path.join(__dirname, '../app/build/')))
 
 app.get('/api/test', (req, res) => res.send('<h1>Test Successful<h2>'))
 
