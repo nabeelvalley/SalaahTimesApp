f7e77b44f6df14320fd3fda0d096e0c3d6d47d05
Nabeel Valley <nabeelvalley@outlook.com>
2019-07-04 21:09:44 +0200

    :sparkles: update masjid model

--- strapi/api/masjid/controllers/Masjid.js
+++ strapi/api/masjid/controllers/Masjid.js
@@ -6,25 +6,11 @@
 
 module.exports = {
     exportText: async (ctx) => {
-
-        const getDisplayTime = (dateString) => {
-            var date = new Date(dateString)
-            var hours = date.getHours()
-            var minutes = date.getMinutes()
-            var ampm = hours >= 12 ? 'pm' : 'am'
-            hours = hours % 12
-            hours = hours ? hours : 12 // the hour '0' should be '12'
-            minutes = minutes < 10 ? '0' + minutes : minutes
-            var strTime = hours + ':' + minutes + ' ' + ampm
-            return strTime
-        }
-
-        // ctx.send("Hello world")
         const masjids = await strapi.services.masjid.find()
 
         const message = masjids.map(masjid =>
             `*${masjid.Name}*
-_${masjid.Address}, ${masjid.Suburb}_${masjid.FajrSalaah ? '\n- Fajr : ' + getDisplayTime(masjid.FajrSalaah) : ''}${masjid.ZuhrSalaah ? '\n- Zuhr: ' + getDisplayTime(masjid.ZuhrSalaah) : ''}${masjid.AsrSalaah ? '\n- Asr: ' + getDisplayTime(masjid.AsrSalaah) : ''}${masjid.EshaSalaah ? '\n- Esha: ' + getDisplayTime(masjid.EshaSalaah) : ''}
+_${masjid.Address}, ${masjid.Suburb}_${masjid.FajrSalaah ? '\n- Fajr : ' + masjid.FajrSalaah : ''}${masjid.ZuhrSalaah ? '\n- Zuhr: ' + masjid.ZuhrSalaah : ''}${masjid.AsrSalaah ? '\n- Asr: ' + masjid.AsrSalaah : ''}${masjid.EshaSalaah ? '\n- Esha: ' + masjid.EshaSalaah : ''}
 `).join('\n')
 
         return message
