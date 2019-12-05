ced42b27d110e94d46cfd7b7c156c1f9f6491200
Nabeel Valley <nabeelvalley@outlook.com>
2019-12-05 19:52:11 +0200

    :wrench:  use two lines for release script

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker build -t salaah-times .
 heroku container:login
-heroku container:push web --app=salaah-times && heroku container:release web  --app=salaah-times
+heroku container:push web --app=salaah-times
+heroku container:release web  --app=salaah-times
