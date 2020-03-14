23c2e07fe0730e0c5fb0fe2050a9be0a86dad75d
Nabeel Valley <nabeelvalley@outlook.com>
2020-03-14 23:03:38 +0200

    :wrench: add netlify.toml

--- strapi/api/masjid/controllers/Masjid.js
+++ strapi/api/masjid/controllers/Masjid.js
@@ -1,13 +1,57 @@
 'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation () to implement custom controller functions
  */
 
 module.exports = {
-  exportText: async (ctx) => {
-    const masjids = await strapi.services.masjid.find()
 
+  /**
+ * Create a record.
+ *
+ * @return {Object}
+ */
+
+  async create(ctx) {
+
+    console.log('Create Masjid')
+
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.restaurant.create(data, { files });
+    } else {
+      entity = await strapi.services.restaurant.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
+
+  /**
+   * Update a record.
+   *
+   * @return {Object}
+   */
+
+  async update(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.restaurant.update(ctx.params, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.restaurant.update(
+        ctx.params,
+        ctx.request.body
+      );
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
+
+
+  exportText: async (ctx) => {
     const message = masjids.map(masjid => {
       const location = masjid.Address && masjid.Suburb
         ? `\n_${masjid.Address}, ${masjid.Suburb}_`
