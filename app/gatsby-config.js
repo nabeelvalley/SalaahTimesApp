f5f4387201f516f39f17aa0ba29467ae871de816
Nabeel Valley <nabeelvalley@outlook.com>
2020-03-31 13:58:13 +0200

    :sparkles: update to work strapi in production

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -1,26 +1,40 @@
-const { resolve } = require('path')
+const fs = require("fs")
+const path = require("path")
+const { buildSchema, buildClientSchema } = require("graphql")
+const { resolve } = require("path")
 
-const strapiBaseUrl = process.env.STRAPI_BASE_URL || 'http://localhost:3001'
+const strapiBaseUrl = process.env.STRAPI_BASE_URL || "http://localhost:3001"
 
-console.log('Strapi Base URL:', strapiBaseUrl)
-
+console.log("Strapi Base URL:", strapiBaseUrl)
 
 module.exports = {
-    siteMetadata: {
-      exportTimesEndpoint: `${strapiBaseUrl}/masjids/export-text`
+  siteMetadata: {
+    exportTimesEndpoint: `${strapiBaseUrl}/masjids/export-text`
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: "Strapi",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "strapi",
+        // Url to query from
+        url: `${strapiBaseUrl}/graphql`,
+        createSchema: async () => {
+          const schemaPath = path.join(
+            __dirname,
+            "../backend/exports/graphql/schema.graphql"
+          )
+
+          console.log(schemaPath)
+
+          const sdl = fs.readFileSync(schemaPath).toString()
+
+          return buildSchema(sdl)
+        }
+      }
     },
-    plugins: [
-        {
-            resolve: 'gatsby-source-graphql',
-            options: {
-                // Arbitrary name for the remote schema Query type
-                typeName: 'Strapi',
-                // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
-                fieldName: 'strapi',
-                // Url to query from
-                url: `${strapiBaseUrl}/graphql`,
-            }
-        },
-        'gatsby-plugin-offline',
-    ]
+    "gatsby-plugin-offline"
+  ]
 }
--- backend/README.md
+++ backend/README.md
@@ -8,4 +8,6 @@ Once ready, run `yarn run develop`
 
 ## Production
 
+Note that the host for development is set to `0.0.0.0` due to a current bug with strapi, next time the application is updated it should be safe to convert this to use localhost
+
 If the application is taking too long to boot on Heroku and is getting "Took too long to bind to port" error take a look at [this](https://devcenter.heroku.com/articles/limits#boot-timeout)
